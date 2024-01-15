c10c39be0f99b17ff70d6efaee939d3bfc9b866f
Vinay Kumar Maheshwaram <54681400+sudovinay01@users.noreply.github.com>
2024-01-15 22:41:23 +0530

    Added calculator test

--- src/test/java/main/my/sudovinay01/sampleproject/winium/CalculatorTest.java
+++ src/test/java/main/my/sudovinay01/sampleproject/winium/CalculatorTest.java
@@ -1,22 +1,65 @@
 package main.my.sudovinay01.sampleproject.winium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+import org.openqa.selenium.winium.WiniumDriverService;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private static final int WINIUM_PORT = 9999;
+    DesktopOptions options;
+    WiniumDriverService service;
+    WiniumDriver driver;
+
     @BeforeTest
-    public void beforeTest() {
+    public void beforeTest() throws IOException {
         winiumUtils.checkDisplay();
+        winiumUtils.killPort(WINIUM_PORT);
+
+        options = new DesktopOptions();
+        options.setApplicationPath("C:\\Windows\\System32\\calc.exe");
+        File driverPath = new File("Winium.Desktop.Driver.exe");
+        //TODO add comments below assignment
+        service = new WiniumDriverService.Builder()
+                .usingDriverExecutable(driverPath)
+                .usingPort(WINIUM_PORT)
+                .withVerbose(false)
+                .withSilent(false)
+                .buildDesktopService();
+        service.start();
+        driver = new WiniumDriver(service, options);
     }
 
     @AfterTest
-    public void afterTest() {
-        System.out.println("After test");
+    public void afterTest() throws IOException {
+        driver.findElement(By.name("Close Calculator")).click();
+        service.stop();
+        winiumUtils.killPort(WINIUM_PORT);
     }
 
     @Test
-    public void test() {
-        System.out.println("current test");
+    public void test() throws InterruptedException {
+        int num1 = 5, num2 = 2;
+        Thread.sleep(1000);
+        System.out.println("number 1 = " + num1
+                + "\nnumber 2 = " + num2);
+        driver.findElement(By.id("clearButton")).click();
+        driver.findElement(By.id("num" + num1 + "Button")).click();
+        driver.findElement(By.name("Multiply by")).click();
+        driver.findElement(By.id("num" + num2 + "Button")).click();
+        driver.findElement(By.id("equalButton")).click();
+        String results = driver.findElement(By.id("CalculatorResults")).getAttribute("Name");
+        System.out.println("Actual value = " + results
+                + "\nExpecting vaule = Display is " + num1 * num2);
+        assertEquals(results, "Display is " + num1 * num2);
     }
 }
--- src/test/java/main/my/sudovinay01/sampleproject/winium/winiumUtils.java
+++ src/test/java/main/my/sudovinay01/sampleproject/winium/winiumUtils.java
@@ -1,5 +1,9 @@
 package main.my.sudovinay01.sampleproject.winium;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import static org.testng.Assert.assertTrue;
 
 public class winiumUtils {
@@ -18,5 +22,25 @@ public class winiumUtils {
      */
     public static void checkDisplay() {
         assertTrue(checkScale(), "\nDisplay scale is not 100%. Set Display scale to 100% for test to begin.\n");
+    }
+
+    /**
+     * Kill processes running on portID. If any.
+     *
+     * @param portID ID of port which tasks need to be killed
+     * @throws IOException Throws IOException
+     */
+    public static void killPort(int portID) throws IOException {
+        String command = "cmd /c netstat -ano | findstr :" + portID;
+        Process process = Runtime.getRuntime().exec(command);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] tokens = line.trim().split("\\s+");
+            String pid = tokens[tokens.length - 1];
+            Runtime.getRuntime().exec("taskkill /F /PID " + pid);
+            if (!pid.equals("0"))
+                System.out.println("Kill process with PID: " + pid);
+        }
     }
 }
